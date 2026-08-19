29aee038b9ae5a62a94569b14e976e26440f7874
Hendrik Röttgers <hrottgers@protonmail.com>
2026-08-19 22:53:24 +0200

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -27,27 +27,99 @@ function Content() {
   return (
     <div>
       <h3>Front End</h3>
-      <p style={{ textAlign: "center", fontSize: "0.8rem" }}>Front end technologies and libraries.</p>
+      <p style={{ textAlign: "center", fontSize: "0.8rem" }}>
+        Front end technologies and libraries.
+      </p>
       <div className="group">
-        <button><a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank" rel="noopener noreferrer">HTML</a></button>
-        <button><a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank" rel="noopener noreferrer">CSS</a></button>
-        <button><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noopener noreferrer">JavaScript</a></button>
+        <button>
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            HTML
+          </a>
+        </button>
+        <button>
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/CSS"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CSS
+          </a>
+        </button>
+        <button>
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            JavaScript
+          </a>
+        </button>
       </div>
       <h3>Back End</h3>
-      <p style={{ textAlign: "center", fontSize: "0.8rem" }}>Back end technologies and libraries.</p>
+      <p style={{ textAlign: "center", fontSize: "0.8rem" }}>
+        Back end technologies and libraries.
+      </p>
       <div className="group">
-        <button><a href="https://developer.mozilla.org/en-US/docs/Web/API" target="_blank" rel="noopener noreferrer">API</a></button>
-        <button><a href="https://developer.mozilla.org/en-US/docs/Web/API/Database" target="_blank" rel="noopener noreferrer">Database</a></button>
-        <button><a href="https://developer.mozilla.org/en-US/docs/Web/API/Server" target="_blank" rel="noopener noreferrer">Server</a></button>
+        <button>
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/API"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            API
+          </a>
+        </button>
+        <button>
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/API/Database"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Database
+          </a>
+        </button>
+        <button>
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/API/Server"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Server
+          </a>
+        </button>
       </div>
       <h3>Programming Languages</h3>
-      <p style={{ textAlign: "center", fontSize: "0.8rem" }}>Syntax and boilerplate.</p>
+      <p style={{ textAlign: "center", fontSize: "0.8rem" }}>
+        Syntax and boilerplate.
+      </p>
       <div className="group">
-        <button><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noopener noreferrer">JavaScript</a></button>
-        <button><a href="https://developer.mozilla.org/en-US/docs/Web/Go" target="_blank" rel="noopener noreferrer">Go</a></button>
+        <button>
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            JavaScript
+          </a>
+        </button>
+        <button>
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/Go"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Go
+          </a>
+        </button>
       </div>
       <h3>Development</h3>
-      <p style={{ textAlign: "center", fontSize: "0.8rem" }}>Development methods.</p>
+      <p style={{ textAlign: "center", fontSize: "0.8rem" }}>
+        Development methods.
+      </p>
       <div className="group">
         <button>Testing</button>
         <button>CI/CD</button>
