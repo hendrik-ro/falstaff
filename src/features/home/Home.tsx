638e74a9e3f5bd6fed118578da613b3927f75f69
Hendrik <hrottgers@protonmail.com>
2026-09-09 22:12:27 +0200

    style: Home + refactor

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setLinks } from "../navBar/navBarSlice";
+import HomeContentFrontEnd from "./HomeFrontEnd";
+import HomeContentBackEnd from "./HomeBackEnd";
+import HomeContentDevelopment from "./HomeDevelopment";
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -46,113 +49,6 @@ function HomeContent() {
       <HomeContentFrontEnd />
       <HomeContentBackEnd />
       <HomeContentDevelopment />
-    </div>
-  );
-}
-
-function HomeContentFrontEnd() {
-  return (
-    <div>
-      <h3>Front End</h3>
-      <p style={{ textAlign: "center", fontSize: "0.8rem" }}>
-        Front end technologies and libraries.
-      </p>
-      <div className="group">
-        <span className="tooltip">
-          <a
-            href="https://developer.mozilla.org/en-US/docs/Web/HTML"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            HTML
-          </a>
-          <span className="tooltiptext">HTML</span>
-        </span>
-        <span className="tooltip">
-          <a
-            href="https://developer.mozilla.org/en-US/docs/Web/CSS"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            CSS
-          </a>
-          <span className="tooltiptext">CSS</span>
-        </span>
-        <span className="tooltip">
-          <a
-            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            JavaScript
-          </a>
-          <span className="tooltiptext">JavaScript</span>
-        </span>
-      </div>
-    </div>
-  );
-}
-
-function HomeContentBackEnd() {
-  return (
-    <div>
-      <h3>Back End</h3>
-      <p style={{ textAlign: "center", fontSize: "0.8rem" }}>
-        Back end technologies and libraries.
-      </p>
-      <div className="group">
-        <span className="tooltip">
-          <a
-            href="https://developer.mozilla.org/en-US/docs/Web/API"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            API
-          </a>
-          <span className="tooltiptext">API</span>
-        </span>
-        <span className="tooltip">
-          <a
-            href="https://developer.mozilla.org/en-US/docs/Web/API/Database"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Database
-          </a>
-          <span className="tooltiptext">Database</span>
-        </span>
-        <span className="tooltip">
-          <a
-            href="https://developer.mozilla.org/en-US/docs/Web/API/Server"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Server
-          </a>
-          <span className="tooltiptext">Server</span>
-        </span>
-      </div>
-    </div>
-  );
-}
-
-function HomeContentDevelopment() {
-  return (
-    <div>
-      <h3>Development</h3>
-      <p style={{ textAlign: "center", fontSize: "0.8rem" }}>
-        Development methods.
-      </p>
-      <div className="group">
-        <span className="tooltip">
-          Testing
-          <span className="tooltiptext">Testing</span>
-        </span>
-        <span className="tooltip">
-          CI/CD
-          <span className="tooltiptext">CI/CD</span>
-        </span>
-      </div>
     </div>
   );
 }
---
+++ src/features/home/HomeFrontEnd.tsx
@@ -0,0 +1,72 @@
+export default function HomeContentFrontEnd() {
+  return (
+    <div>
+      <h3>Front End</h3>
+      <p style={{ textAlign: "center", fontSize: "0.8rem" }}>
+        Front end technologies and libraries.
+      </p>
+      <div className="group">
+        <span className="tooltip">
+          <a
+            href="https://react.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span className="tooltiptext">React`s official website</span>
+        </span>
+        <span className="tooltip">
+          <a
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          <span className="tooltiptext">Redux Toolkit`s official website</span>
+        </span>
+        <span className="tooltip">
+          <p>Frameworks</p>
+          <span className="tooltiptext">Not yet implemented</span>
+        </span>
+        <span className="tooltip">
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            HTML
+          </a>
+          <span className="tooltiptext">
+            Mozilla Developer Network`s documentation
+          </span>
+        </span>
+        <span className="tooltip">
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/CSS"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CSS
+          </a>
+          <span className="tooltiptext">
+            Mozilla Developer Network`s documentation
+          </span>
+        </span>
+        <span className="tooltip">
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            JavaScript
+          </a>
+          <span className="tooltiptext">
+            Mozilla Developer Network`s documentation
+          </span>
+        </span>
+      </div>
+    </div>
+  );
+}
