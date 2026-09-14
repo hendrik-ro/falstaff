d46540d17523153555b1896f7ca4358500c40c44
Hendrik <hrottgers@protonmail.com>
2026-09-14 13:34:34 +0200

    Redux typed code snippets

--- src/features/frontend/redux/Store.tsx
+++ src/features/frontend/redux/Store.tsx
@@ -4,6 +4,10 @@ export default function ReduxStore() {
   return (
     <div>
       <h2>Redux Store</h2>
+      <p>
+        Create the Redux store file <code>store.ts</code> using{" "}
+        <code>configureStore</code>.
+      </p>
       <Syntax
         language="typescript"
         code={`import { configureStore } from '@reduxjs/toolkit'
@@ -27,6 +31,34 @@ export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export default store;
 `}
+        lineNumbers={true}
+      />
+      <p>
+        Wrap the <code>App</code> component with <code>Provider</code> to make
+        the store available to all components:
+      </p>
+      <Syntax
+        language="typescript"
+        code={`import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./app/App.tsx";
+import store from "./app/store";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <AppWrapper />
+  </StrictMode>,
+);
+
+export function AppWrapper() {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}`}
+        lineNumbers={true}
       />
     </div>
   );
